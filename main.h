fa672d879c1b56b42329320db9907dfb41dac9d3
Mustaphajafari <jaafarimustapha28@gmail.com>
2023-07-24 16:45:52 -0700

    printf

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int handle_number(int num);
+int len_num(int num);
+int handle_string(char *str);
+
+#endif
