320e7118472d3b6629fe654ad6868f713736409c
Mustaphajafari <jaafarimustapha28@gmail.com>
2023-07-25 09:28:45 -0700

    l

--- main.h
+++ main.h
@@ -6,5 +6,5 @@ int _printf(const char *format, ...);
 int handle_number(int num);
 int len_num(int num);
 int handle_string(char *str);
-
+int handle_format(const char *format, va_list list);
 #endif
