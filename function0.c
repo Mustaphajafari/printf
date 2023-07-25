51c5804d408b955b4b1f855037cab19bdbec428f
Hichammayat <hichammayat07@gmail.com>
2023-07-25 03:32:19 -0700

    mandatory tasks

---
+++ function0.c
@@ -0,0 +1,96 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * handle_string - Prints a string or "null" if str is NULL.
+ * @str: The string to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+
+int handle_string(char *str)
+{
+	if (str == NULL)
+	{
+		const char *null_str = "null";
+		int i = 0;
+		while (null_str[i])
+		{
+			_putchar(null_str[i]);
+			i++;
+		}
+		return (i);
+	}
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * len_num - Calculates the length of an integer.
+ * @num: The input integer.
+ *
+ * Return: The number of digits in the integer.
+ */
+
+int len_num(int num)
+{
+	if (num == 0)
+		return 1;
+	int count = 0;
+	if (num < 0)
+	{
+		count++;
+		num = -num;
+	}
+	while (num != 0)
+	{
+		count++;
+		num /= 10;
+	}
+	return count;
+}
+
+/**
+ * handle_number - Prints an integer.
+ * @num: The integer to be printed.
+ *
+ * Return: Always 1.
+ */
+
+int handle_number(int num)
+{
+	if (num == -2147483648)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if (num >= 10)
+	{
+		handle_number(num / 10);
+		handle_number(num % 10);
+	}
+	else
+	{
+		_putchar(num + '0');
+	}
+	return (1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ printf.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <main.h>
+#include <stdarg.h>
+/**
+ * _printf - Printf function
+ * @format: String charactere.
+ * Return: chars printed.
+ */
+int _printf(char *format, ...)
+{
+        int num_chars = 0;
+        va_list list;
+        if(!format || format[0] == '\0')
+                return (-1);
+        va_start (list, format);
+        char c;
+        const char *s;
+        while (*format)
+	{
+		if(*format == "%")
+                {
+                        format++;
+                        switch (*format)
+                        {
+                                case 'c':
+                                {
+                                        char c = va_arg(list, int);
+                                        num_chars = num_chars + _putchar(c);
+                                        break;
+                                }
+                                case 's':
+                                {
+                                        const char *str = va_arg(list, const char *);
+                                        num_chars = num_chars + handle_string(str);
+                                        break;
+                                }
+				case '%':
+                                {
+                                        _putchar('%');
+                                        num_chars++;
+                                        break;
+                                }
+                                case 'd':
+                                case 'i':
+                                {
+                                        int num = va_arg(args, int);
+                                        if (num < 0)
+                                        {
+                                                num_chars++;
+                                        }
+                                        num_chars = num_chars + len_num(num);
+                                        handle_number(num);
+                                        break;
+                                }
+				default:
+                                {
+                                        _putchar('%');
+                                        num_chars++;
+                                        if (*format)
+                                        {
+                                                _putchar(*format);
+                                                num_chars++;
+                                        }
+                                        break;
+                                }
+
+
+
+                }
+                format++;
+                }
+
+
+                else
+                {
+                        _putchar(*format);
+                        format++;
+                        num_chars++;
+                }
+	}
+
+
+va_end(list);
+return num_chars;
+
+}
+
