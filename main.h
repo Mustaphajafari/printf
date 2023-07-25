49b8a57e89df242555591a3dbfe5782096503eda
Mustaphajafari <jaafarimustapha28@gmail.com>
2023-07-25 09:54:28 -0700

    k

--- main.h
+++ main.h
@@ -7,4 +7,8 @@ int handle_number(int num);
 int len_num(int num);
 int handle_string(char *str);
 int handle_format(const char *format, va_list list);
+int handle_char_format(va_list list);
+int handle_string_format(va_list list);
+int handle_decimal_format(va_list list);
+
 #endif
