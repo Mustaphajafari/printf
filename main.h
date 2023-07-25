7cb35e2e599f3c9ab1afcec39c71c4427dbab8ef
Mustaphajafari <jaafarimustapha28@gmail.com>
2023-07-25 13:22:14 -0700

    h

--- main.h
+++ main.h
@@ -3,12 +3,16 @@
 #include <stdarg.h>
 int _putchar(char c);
 int _printf(const char *format, ...);
+
 int handle_number(int num);
 int len_num(int num);
 int handle_string(const char *str);
-int handle_format(const char *format, va_list list);
+
+
 int handle_char_format(va_list list);
 int handle_string_format(va_list list);
 int handle_decimal_format(va_list list);
+
+
 
 #endif
